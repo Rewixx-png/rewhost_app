10dc619e50595d3b24585741b9f4f914fae2fc18
kmoell <dolyakypiaezy@gmail.com>
2026-01-01 22:04:35 +0300

    fix: remove duplicate startKoin from MainActivity

--- composeApp/src/androidMain/kotlin/com/rewhost/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/rewhost/app/MainActivity.kt
@@ -5,30 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.rewhost.app.di.appModule
-import com.rewhost.app.utils.PlatformSettingsFactory
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
-import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         
-        // Инициализация Koin с Android Context
-        startKoin {
-            androidContext(this@MainActivity)
-            modules(appModule, androidModule())
-        }
-
+        // Koin is already initialized in RewHostApplication
+        
         setContent {
             App()
         }
     }
-}
-
-fun androidModule() = module {
-    single { PlatformSettingsFactory(get()) }
 }
 
 @Preview
